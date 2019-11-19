f2838483f153230311ae6bd3dae22b87bfebabd2
LvQi <CreeperCl2@outlook.com>
2019-11-19 11:07:50 +0800

    rt

--- 抱怨量.py
+++ 抱怨量.py
@@ -3,7 +3,7 @@
 ### Version: Python 3.7.4
 ### Date: 2019-11-18 16:50:21
 ### LastEditors: ChlorineLv@outlook.com
-### LastEditTime: 2019-11-18 17:26:25
+### LastEditTime: 2019-11-19 09:39:00
 ### Description: 计算抱怨量
 '''
 
@@ -34,7 +34,7 @@ def get_name_list():
     return df.to_dict(orient='index')
 
 
-def get_bao_list(excel_file, excel_sheet, column_judge, column_name):
+def get_bao_list(excel_file, excel_sheet, column_name, column_judge):
     '''
     ### description: 以工号为key的各人抱怨/绿通次数
     ### param { (str)excel文件所在位置, (str)sheet名称, (str)用于筛选的列名, (str)工号列名 } 
@@ -44,7 +44,7 @@ def get_bao_list(excel_file, excel_sheet, column_judge, column_name):
     # excel_file = input(f'请输入《抱怨清单统计2019年10月》文件名：（默认：{excel_file}）\n').strip() or excel_file
     print(f'正在处理:{excel_file}')
     # 下载下来的excel像是gbk格式
-    df = pd.DataFrame(pd.read_excel(excel_file, sheet_name=excel_sheet))[[column_judge, column_name]]
+    df = pd.DataFrame(pd.read_excel(excel_file, sheet_name=excel_sheet))[[column_name, column_judge]]
     # 以处理人工号计算频次
     df = df.groupby(by=column_name).size()
     # 先转为dataframe再转为dict
@@ -56,12 +56,13 @@ if __name__ == "__main__":
     dict_name = get_name_list()
     # 获取抱怨量
     file_baoyuan = f'{os.path.dirname(__file__)}\抱怨清单统计2019年10月.xlsx'
-    dict_baoyuan = get_bao_list(file_baoyuan, '清单', '', '受理人工号')
+    dict_baoyuan = get_bao_list(file_baoyuan, '清单', '工号', '服务类别')
     file_lvtong = f'{os.path.dirname(__file__)}\绿通单清单2019年10月.xlsx'
-    dict_lvtong = get_bao_list(file_lvtong, '1', '', '处理人工号')
+    dict_lvtong = get_bao_list(file_lvtong, '1', '处理人工号', '服务类型')
     n = 0
     for i in dict_name:
-
+        i1 = i[1:] if i.startswith('0') else i
+        dict_name[i]['抱怨'] = dict_baoyuan.get(i1, {0:0})[0] + dict_lvtong.get(i1, {0:0})[0]
         
         
         n+=1
@@ -69,4 +70,4 @@ if __name__ == "__main__":
     df = pd.DataFrame.from_dict(dict_name, orient='index')
     print(df)
     temp_excel_file = f'{os.path.dirname(__file__)}\中间表：人员产能表：{time.strftime("%Y-%m-%d", time.localtime())}.xlsx'
-    # df.to_excel(excel_writer = temp_excel_file, index = True)
+    df.to_excel(excel_writer = temp_excel_file, index = True)
